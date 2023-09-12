3b2398053ecdd6f4c74a3d0bdd55decc36063ba8
Guan-Yu Lin <guanyulin@google.com>
2023-09-12 18:04:01 +0800

    aoc: add new a state to indicate aoc ssr
    Add a new state to indicate AoC is under SSR. This prevents other
    drivers access aoc services during AoC SSR.
    
    Bug: 298015705
    Test: Local AoC SSR multiple times.
    Change-Id: I615adf38f7ae3f64ee076d11d4b5e59d6cb3d8e9
    Signed-off-by: Guan-Yu Lin <guanyulin@google.com>

--- aoc.c
+++ aoc.c
@@ -852,7 +852,7 @@ static ssize_t services_show(struct device *dev, struct device_attribute *attr,
 	int i;
 
 	atomic_inc(&prvdata->aoc_process_active);
-	if (aoc_state != AOC_STATE_ONLINE || work_busy(&prvdata->watchdog_work))
+	if (aoc_state != AOC_STATE_ONLINE)
 		goto exit;
 
 	ret += scnprintf(buf, PAGE_SIZE, "Services : %d\n", services);
@@ -940,7 +940,7 @@ static ssize_t reset_store(struct device *dev, struct device_attribute *attr,
 	char reason_str[MAX_RESET_REASON_STRING_LEN + 1];
 	size_t reason_str_len = min(MAX_RESET_REASON_STRING_LEN, count);
 
-	if (aoc_state != AOC_STATE_ONLINE || work_busy(&prvdata->watchdog_work)) {
+	if (aoc_state != AOC_STATE_ONLINE) {
 		dev_err(dev, "Reset requested while AoC is not online");
 		return -ENODEV;
 	}
@@ -978,7 +978,6 @@ static ssize_t force_reload_store(struct device *dev, struct device_attribute *a
 
 	strlcpy(prvdata->ap_reset_reason, "Force Reload AoC", AP_RESET_REASON_LENGTH);
 	prvdata->ap_triggered_reset = true;
-
 	schedule_work(&prvdata->watchdog_work);
 
 	return count;
@@ -1536,7 +1535,7 @@ static void aoc_take_offline(struct aoc_prvdata *prvdata)
 	int rc;
 
 	/* check if devices/services are ready */
-	if (aoc_state == AOC_STATE_ONLINE) {
+	if (aoc_state == AOC_STATE_ONLINE || aoc_state == AOC_STATE_SSR) {
 		pr_notice("taking aoc offline\n");
 		aoc_state = AOC_STATE_OFFLINE;
 
@@ -1600,7 +1599,7 @@ static void aoc_process_services(struct aoc_prvdata *prvdata, int offset)
 
 	atomic_inc(&prvdata->aoc_process_active);
 
-	if (aoc_state != AOC_STATE_ONLINE || work_busy(&prvdata->watchdog_work))
+	if (aoc_state != AOC_STATE_ONLINE)
 		goto exit;
 
 	services = aoc_num_services();
@@ -1689,6 +1688,7 @@ static void aoc_watchdog(struct work_struct *work)
 	struct aoc_section_header *crash_info_section =
 		find_ramdump_section(ramdump_header, SECTION_TYPE_CRASH_INFO);
 
+	aoc_state = AOC_STATE_SSR;
 	prvdata->total_restarts++;
 
 	/* Initialize crash_info[0] to identify if it has changed later in the function. */
@@ -2157,7 +2157,7 @@ static int aoc_core_suspend(struct device *dev)
 	int i = 0;
 
 	atomic_inc(&prvdata->aoc_process_active);
-	if (aoc_state != AOC_STATE_ONLINE || work_busy(&prvdata->watchdog_work))
+	if (aoc_state != AOC_STATE_ONLINE)
 		goto exit;
 
 	for (i = 0; i < total_services; i++) {
@@ -2180,7 +2180,7 @@ static int aoc_core_resume(struct device *dev)
 	int i = 0;
 
 	atomic_inc(&prvdata->aoc_process_active);
-	if (aoc_state != AOC_STATE_ONLINE || work_busy(&prvdata->watchdog_work))
+	if (aoc_state != AOC_STATE_ONLINE)
 		goto exit;
 
 	for (i = 0; i < total_services; i++) {
