GrapheneOS-Archive/kernel_google-modules_aoc-zuma

aoc: usb: improve usb recovery during aoc ssr
The original recovery flow didn't cover the case when usb connection
changes during AoC SSR. Instead of changing id to turn on/off host mode,
this fix uses fsm_reset so we can monitor id information during aoc
restart.

Bug: 297480242
Test: The following 5 tests passed:
  1. AoC SSR during playback/capture.
  2. Headset plugged-in/plugged-out during AoC SSR.
  3. AoC SSR during gadget mode (ADB connection).
  4. Restart pixel with host mode engaged.
  5. Restart pixel with gadget mode engaged.
Change-Id: I9b0475a0d75181cbf09cea7dc2ae34511c0a0bc1
Signed-off-by: Guan-Yu Lin <guanyulin@google.com>

usb/aoc_usb.h
@@ -28,12 +28,6 @@ enum aoc_usb_state {
 	USB_CONNECTED
 };
 
-enum usb_recover_state {
-	NONE,
-	RECOVER_HOST_OFF,
-	RECOVER_HOST_ON,
-	RECOVERED
-};
 
 struct aoc_usb_drvdata {
 	struct aoc_service_dev *adev;
@@ -44,7 +38,6 @@ struct aoc_usb_drvdata {
 	struct notifier_block nb;
 
 	long service_timeout;
-	unsigned int usb_conn_state;
 };
 
 struct conn_stat_args {
@@ -88,8 +81,7 @@ bool is_aoc_usb_probe_done(void);
 int xhci_offload_helper_init(void);
 int usb_vendor_helper_init(void);
 
-extern int dwc3_otg_host_enable(bool enabled);
-
+extern int dwc3_otg_fsm_try_reset(bool enabled);
 extern bool aoc_alsa_usb_capture_enabled(void);
 extern bool aoc_alsa_usb_playback_enabled(void);
 

oberdfr/google-modules_aoc

Test: The following 6 tests passed:
  1. AoC SSR during USB_ROLE_NONE.
  2. AoC SSR during USB_ROLE_HOST.
  3. AoC SSR during USB_ROLE_DEVICE.
  4. Role changed during AoC SSR.
  5. Restart pixel with USB_ROLE_HOST engaged.
  6. Restart pixel with USB_ROLE_DEVICE engaged.

usb/aoc_usb.h
@@ -28,12 +28,6 @@ enum aoc_usb_state {
 	USB_CONNECTED
 };
 
-enum usb_recover_state {
-	NONE,
-	RECOVER_HOST_OFF,
-	RECOVER_HOST_ON,
-	RECOVERED
-};
 
 struct aoc_usb_drvdata {
 	struct aoc_service_dev *adev;
@@ -44,7 +38,6 @@ struct aoc_usb_drvdata {
 	struct notifier_block nb;
 
 	long service_timeout;
-	unsigned int usb_conn_state;
 };
 
 struct conn_stat_args {
@@ -83,13 +76,10 @@ int usb_host_mode_state_notify(enum aoc_usb_state usb_state);
 int xhci_set_isoc_tr_info(u16 ep_id, u16 dir, struct xhci_ring *ep_ring);
 int xhci_get_usb_audio_count(void);
 
-bool is_aoc_usb_probe_done(void);
-
 int xhci_offload_helper_init(void);
 int usb_vendor_helper_init(void);
 
-extern int dwc3_otg_host_enable(bool enabled);
-
+extern int dwc3_otg_host_ready(bool enabled);
 extern bool aoc_alsa_usb_capture_enabled(void);
 extern bool aoc_alsa_usb_playback_enabled(void);
 